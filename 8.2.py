9224a75fda6629637bde36485bf1ec3f2d97aeb1
CoolBeanMike <mikebova@verizon.net>
2018-02-11 13:31:59 -0500

    first script.

--- 8.2.py
+++ 8.2.py
@@ -2,6 +2,20 @@ numlist = list()
 
 while True:
     inp = input('Input a number here: ')
-    if inp == 'done': break
+    if inp == 'done':break
     value = float(inp)
     numlist.append(value)
+print(numlist)
+
+average = sum(numlist) / len(numlist)
+
+print(average)
+
+
+abc = 'cool one two dsa sad f erdf f sdf sdf sdf ds'
+abc = abc.split()
+print(abc)
+print(abc[4])
+
+for x in abc:
+    print(x)
