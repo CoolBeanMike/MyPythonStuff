92a9c38674dde8db0276682a051325e775026cf3
Michael <coolbeanmike@gmail.com>
2025-07-22 19:50:22 -0400

    first script.

--- mongo_jason_import_script.py
+++ mongo_jason_import_script.py
@@ -5,11 +5,12 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 
 # Create/use a database and collection
-db = client["mydatabase"]
+db = client["cool1"]
 collection = db["people"]
 
 # Load JSON data from file
-with open("data.json") as f:
+#with open("data.json") as f:
+with open(r"C:\Users\Michael\Downloads\test_db.users2.json") as f:
     data = json.load(f)
 
 # Insert data into MongoDB
